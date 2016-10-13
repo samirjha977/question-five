07545fbe9af5f52b65b18de800490265ae1513f2
Samir Jha <samirjha642@gmail.com>
2016-10-12 20:57:31 -0500

    files

---
+++ ten-files.sh
@@ -0,0 +1,6 @@
+touch crumpets{01..10}.txt 
+#echo "he crags are the best part!">> crumpets{01-05}.txt
+#echo "My present working directory is: your-$PWD-here" >> crumpets{06-10}.txt
+
+
+                 
